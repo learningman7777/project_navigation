f5a693cca3242dad68f2c18298e55474d507cd01
promotionk <cancan21st@gmail.com>
2019-06-16 16:32:31 +0900

    modified DQN -> Double DQN

--- dqn_agent.py
+++ dqn_agent.py
@@ -92,8 +92,8 @@ class Agent():
         """
         states, actions, rewards, next_states, dones, w, indices  = experiences
 
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
-        
+        best_actions = self.qnetwork_local(next_states).max(1)[1].unsqueeze(1)
+        Q_targets_next = self.qnetwork_target(next_states).detach().gather(1, best_actions)
         # Compute Q targets for current states
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
